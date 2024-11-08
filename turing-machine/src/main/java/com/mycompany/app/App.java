34432b71045190da278ed135856030297fd87a0b
Jose-Prince <joseprince148@gmail.com>
2024-11-07 19:26:57 -0600

    Feat: JSON lecture for turing machine

--- turing-machine/src/main/java/com/mycompany/app/App.java
+++ turing-machine/src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,49 @@
 package com.mycompany.app;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+	Scanner scan = new Scanner(System.in);	
+
+	Gson gson = new Gson();
+
+	List<String> states = new ArrayList<>();
+	List<String> alphabet = new ArrayList<>();
+	String initial_state = "";
+	List<String> final_states = new ArrayList<>();
+	Map<String, Object> transitions = new HashMap<>(); 
+	System.out.println("Encriptador/Desencriptador de cifrado césar");
+	System.out.println("\nEscoja una de las siguientes opciones:\n(1) Encriptar\n(2) Desencriptar");
+	System.out.println("Opción: ");
+	int option = scan.nextInt();
+	scan.nextLine();
+
+
+	if (option == 1) {
+		try (FileReader reader = new FileReader("encrypt.json")) {
+			Machine machine = gson.fromJson(reader, Machine.class);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	} else {
+		try (FileReader reader = new FileReader("/desencrypt.json")) {
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
     }
 }
---
+++ turing-machine/src/main/java/com/mycompany/app/Machine.java
@@ -0,0 +1,69 @@
+package com.mycompany.app;
+
+import java.util.List;
+
+public class Machine {
+    public List<String> Q; //Estados
+    public List<String> Σ; //Alfabeto de entrada
+    public List<String> Γ; //Alfabeto de cinta
+    public String q0; //Estado inicial
+    public List<String> F; //Estados finales
+    public String Blanc; //Símbolo blanco
+    public List<List<List<Object>>> δ; //Lista de transiciones
+
+    public List<String> getQ() {
+        return Q;
+    }
+
+    public void setQ(List<String> q) {
+        this.Q = q;
+    }
+
+    public List<String> getΣ() {
+        return Σ;
+    }
+
+    public void setΣ(List<String> Σ) {
+        this.Σ = Σ;
+    }
+
+    public String getΓ() {
+        return q0;
+    }
+
+    public void setΓ(List<String> Γ) {
+        this.Γ = Γ;
+    }
+
+    public String getQ0() {
+        return q0;
+    }
+
+    public void SetQ0(String q0) {
+        this.q0 = q0;
+    }
+
+    public List<String> getF() {
+        return F;
+    }
+
+    public void setF(List<String> F) {
+        this.F = F;
+    } 
+
+    public String getBlanc() {
+        return Blanc;
+    }
+
+    public void setBlanc(String blanc) {
+        this.Blanc = blanc;
+    }
+
+    public List<List<List<Object>>> getDelta() {
+        return δ;
+    }
+
+    public void setDelta(List<List<List<Object>>> δ) {
+        this.δ = δ;
+    }
+}
