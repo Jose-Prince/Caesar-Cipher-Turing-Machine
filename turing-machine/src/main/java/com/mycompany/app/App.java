1abfb3af880c939d8a99735d73bded38cc270914
Jose-Prince <joseprince148@gmail.com>
2024-11-11 17:57:30 -0600

    Feat: Input for string and k

--- turing-machine/src/main/java/com/mycompany/app/App.java
+++ turing-machine/src/main/java/com/mycompany/app/App.java
@@ -3,9 +3,7 @@ package com.mycompany.app;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
@@ -18,29 +16,35 @@ public class App {
 	Scanner scan = new Scanner(System.in);	
 
 	Gson gson = new Gson();
+	System.out.println("Encriptador/Desencriptador de cifrado césar");
+
+	//Sets input
+	System.err.print("\nIngrese el texto que quiere encriptar/desencriptar: ");
+	String input = scan.nextLine();
+	//Sets k
+	System.out.print("Ingrese el valor k: ");
+	int k = scan.nextInt();
+	scan.nextLine();
 
 	List<String> states = new ArrayList<>();
 	List<String> alphabet = new ArrayList<>();
 	String initial_state = "";
 	List<String> final_states = new ArrayList<>();
-	Map<String, Object> transitions = new HashMap<>(); 
-	System.out.println("Encriptador/Desencriptador de cifrado césar");
+	List<List<List<Object>>> transitions = new ArrayList<>(); 
 	System.out.println("\nEscoja una de las siguientes opciones:\n(1) Encriptar\n(2) Desencriptar");
-	System.out.println("Opción: ");
+	System.out.print("Opción: ");
 	int option = scan.nextInt();
 	scan.nextLine();
-
 
 	if (option == 1) {
 		try (FileReader reader = new FileReader("encrypt.json")) {
 			Machine machine = gson.fromJson(reader, Machine.class);
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	} else {
-		try (FileReader reader = new FileReader("/desencrypt.json")) {
-			
+		try (FileReader reader = new FileReader("desencrypt.json")) {
+			Machine machine = gson.fromJson(reader, Machine.class);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
